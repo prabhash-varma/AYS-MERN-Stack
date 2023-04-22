1bf8b458e98827a26f630fa4e0298d4830b15d06
Prabhash Varma <varmaprabhash@gmail.com>
2023-04-23 00:36:37 +0530

    Update

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -28,7 +28,7 @@ const handleFileUpload = async (e) => {
     const file = e.target.files[0];
     const base64 = await convertToBase64(file);
     setImageurl(base64);
-    Change(file);
+    //Change(file);
   };
 
  
